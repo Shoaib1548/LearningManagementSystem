ef8811d9e3ad8be533683d15b0e32205338dc98d
Shoaib1548 <khanshoaib1548@gmail.com>
2026-09-10 13:53:11 +0530

    Update v1.1-next

--- LEMS.py
+++ LEMS.py
@@ -1,10 +1,20 @@
 courses = ["Python", "Java", "Web Development"]
 
-print("Learning Management System")
+name = input("Enter your name: ")
 
 for i, course in enumerate(courses, 1):
     print(i, course)
 
 choice = int(input("Choose a course: "))
+course = courses[choice - 1]
 
-print("You enrolled in:", courses[choice - 1])
+progress = int(input("Enter progress (%): "))
+
+print("\nStudent:", name)
+print("Course:", course)
+print("Progress:", progress, "%")
+
+if progress == 100:
+    print("Course Completed!")
+else:
+    print("Course In Progress")
